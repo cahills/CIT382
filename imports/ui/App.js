792d41c15f0a420bdb96f2efe4d211a909268ae4
cahills@uoregon.edu <cahills@uoregon.edu>
2018-02-04 21:00:05 -0800

    Tempalate for asthetic confirmed.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -7,20 +7,31 @@ export default class App extends Component {
       this.state= {
         tracktext: "",
         secretkey: "",
+        value: "",
       };
     }
 
     onTextEntered = (changeEvent) => {
       let text = changeEvent.target.value;
       this.setState({tracktext: text});
-      console.log(this.state);
+      console.log(changeEvent.target.value);
     }
 
     onSecretEntered = (changeKey) => {
       let key = changeKey.target.value;
       this.setState({secrectkey: key});
-      console.log(this.state);
+      console.log(changeKey.target.value);
     }
+
+    onValueEntered = (changeValue) => {
+      let value = changeValue.target.value;
+      this.setState({
+        value: value
+      });
+      console.log(changeValue.target.value);
+    }
+
+
 
   render(){
     return(
@@ -28,17 +39,19 @@ export default class App extends Component {
         <h1>Cypher</h1>
         <div>
 
-          <input  className ="Text" type = "input" onChange={this.onTextEntered.bind(this)}/>
+          <input  className ="Text" type = "input" onChange={this.onTextEntered.bind(this)} placeholder="Input message please..."/>
 
           <br/>
           <br/>
 
-          <input type = "input" onChange={this.onSecretEntered.bind(this)}/>
+          <input type = "input" onChange={this.onSecretEntered.bind(this)} placeholder="What is your key?"/>
           <h2>Caesar</h2>
 
           <h3> CipherText</h3>
 
-           <input type = "input"/>
+          <input type = "input" value={this.state.value} onChange={this.onValueEntered.bind(this)} />
+           <input type="range" min="0" max="9" className="slider" id="myRange" value={this.state.value}/>
+          Shift Left:<input type="checkbox" name="shiftLeft" defaultValue="Left" />
 
 
            <h2>Cypto AES</h2>
